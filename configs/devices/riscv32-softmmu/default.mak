a91a0f594de00a59e93c7d1df1a05300fab26948
ddrcode <ddrcode@gmail.com>
2025-01-11 23:12:04 +0000

    retro machine definition

--- configs/devices/riscv32-softmmu/default.mak
+++ configs/devices/riscv32-softmmu/default.mak
@@ -5,8 +5,10 @@
 # CONFIG_TEST_DEVICES=n
 
 # Boards are selected by default, uncomment to keep out of the build.
-# CONFIG_SPIKE=n
-# CONFIG_SIFIVE_E=n
-# CONFIG_SIFIVE_U=n
+CONFIG_SPIKE=n
+CONFIG_SIFIVE_E=n
+CONFIG_SIFIVE_U=n
 # CONFIG_RISCV_VIRT=n
-# CONFIG_OPENTITAN=n
+CONFIG_OPENTITAN=n
+CONFIG_RETRO=y
+
---
+++ hw/riscv/retro.c
@@ -0,0 +1,56 @@
+#include "qemu/osdep.h"
+#include "qemu/units.h"
+#include "qemu/error-report.h"
+#include "qemu/guest-random.h"
+#include "qapi/error.h"
+#include "hw/boards.h"
+#include "hw/loader.h"
+#include "hw/sysbus.h"
+#include "hw/qdev-properties.h"
+#include "hw/char/serial-mm.h"
+#include "target/riscv/cpu.h"
+#include "hw/core/sysbus-fdt.h"
+#include "target/riscv/pmu.h"
+#include "hw/riscv/riscv_hart.h"
+#include "hw/riscv/iommu.h"
+#include "hw/riscv/riscv-iommu-bits.h"
+#include "hw/riscv/virt.h"
+#include "hw/riscv/boot.h"
+#include "hw/riscv/numa.h"
+#include "kvm/kvm_riscv.h"
+#include "hw/firmware/smbios.h"
+#include "hw/intc/riscv_aclint.h"
+#include "hw/intc/riscv_aplic.h"
+#include "hw/intc/sifive_plic.h"
+#include "hw/misc/sifive_test.h"
+#include "hw/platform-bus.h"
+#include "chardev/char.h"
+#include "system/device_tree.h"
+#include "system/system.h"
+#include "system/tcg.h"
+#include "system/kvm.h"
+#include "system/tpm.h"
+#include "system/qtest.h"
+#include "hw/pci/pci.h"
+#include "hw/pci-host/gpex.h"
+#include "hw/display/ramfb.h"
+#include "hw/acpi/aml-build.h"
+#include "qapi/qapi-visit-common.h"
+#include "hw/virtio/virtio-iommu.h"
+
+static void retro_init(MachineState *machine)
+{
+//   RISCVCPU *cpu = RISCV_CPU(cpu_create(machine->cpu_type));
+//   riscv_add_cpu(cpu, machine);  // Ensure this function is correctly called
+//   memory_region_allocate(get_system_memory(), 0x80000000, 0x100000);  // Example memory allocation
+}
+
+static void retro_machine_init(MachineClass *mc) {
+    mc->desc = "RISC-V Retro Machine";
+    mc->init = retro_init;
+    mc->max_cpus = 1;
+    mc->is_default = true;
+    mc->default_ram_id = "ram";
+}
+
+DEFINE_MACHINE("retro", retro_machine_init)
